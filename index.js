c44f99fd4a3be10d5d663efe26203f11ecc4ba42
SanazSyavashi <sanawzsyavashi@gmail.com>
2025-02-23 17:49:57 +0330

    fix

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors({
   origin:true,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization','Accept'],
   exposedHeaders: ['Content-Range', 'X-Content-Range'],
   maxAge: 600,
   credentials: true
